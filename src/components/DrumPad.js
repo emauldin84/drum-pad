7c2ded612ec0444886d06f814a595e2de018b219
Eric Mauldin <emauldin84@gmail.com>
2019-11-13 21:43:12 -0500

    initial commit. adds components folder and drum-pad component

---
+++ src/components/DrumPad.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const DrumPad = () => {
+
+    return(
+        <div className='drum-pad-container'>
+            <div className='drum-pad' style={{border: '1px solid black', height: '500px', width: '500px'}}>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default DrumPad
